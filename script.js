7ae49433169583b0e65df08af5e8732855e02944
draxo3 <accback44@gmail.com>
2025-05-30 19:28:23 +0200

    Update script.js

--- script.js
+++ script.js
@@ -15,7 +15,7 @@ function detectBrowser() {
   // Accept all the listed browsers, even mobile variants
   if (uaLower.includes("crios") || uaLower.includes("chrome")) return "Chrome";
   if (uaLower.includes("fxios") || uaLower.includes("firefox")) return "Firefox";
-  if (uaLower.includes("edgios") || uaLower.includes("edge")) return "Edge";
+ 
   if (uaLower.includes("brave")) return "Brave";
   if (uaLower.includes("opera gx")) return "Opera GX";
   if (uaLower.includes("opera") || uaLower.includes("opr")) return "Opera";

5c8499bdbe0f9f5944bbe5be9d78cb658ee204cd
draxo3 <accback44@gmail.com>
2025-05-30 19:25:13 +0200

    Update script.js

--- script.js
+++ script.js
@@ -55,11 +55,11 @@ function showIntro() {
 window.addEventListener('DOMContentLoaded', () => {
   // Mobile device detection
   if (isMobileDevice()) {
-    document.getElementById('bvVerifyingText').innerText = "Sorry but Mobile is not supported";
+    document.getElementById('bvVerifyingText').innerText = "Sorry but Mobile is not supported Orbital Supports Pc,laptop only";
     document.getElementById('bvVerifyingText').style.color = "#ff9100";
-    document.getElementById('bvSubtitle').innerText = "Please switch on a PC or tablett";
+    document.getElementById('bvSubtitle').innerText = "Please switch on a PC";
     document.getElementById('bvCheckBox').style.opacity = 0.7;
-    document.getElementById('bvDesc').innerText = "Orbital.exe cannot continue on mobile devices. Please use a PC or tablet.";
+    document.getElementById('bvDesc').innerText = "Orbital.exe cannot continue on mobile devices. Please use a PC or LapTop.";
     return;
   }
 
